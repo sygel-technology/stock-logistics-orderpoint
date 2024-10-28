92557a6d6fc1efb1f9d23121f6efa532f54a9f45
manu <manuel.regidor@sygel.es>
2024-10-28 15:57:36 +0100

    [MIG]stock_orderpoint_move_link: Migration to 17.0

--- stock_orderpoint_move_link/__manifest__.py
+++ stock_orderpoint_move_link/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Orderpoint Move Link",
     "summary": "Link Reordering rules to stock moves",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/stock-logistics-orderpoint",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
